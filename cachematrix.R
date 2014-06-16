cce32a8e64693d058f9b31f5db819e6c8fec9cbd
unknown <sksharma@LTEWR-SKSHARMA.corp.ad.broadcom.com>
2014-06-16 16:07:04 -0400

    updated code

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,65 @@
-## Put comments here that give an overall description of what your
-## functions do
+# R Programming - Coursera
+# Programming Assignment 2
 
-## Write a short comment describing this function
+# makeCacheMatrix: This function creates a special "matrix" object that can cache its inverse.
+# It has four subfunctions: 
+# set --> set a new matrix value
+# get --> get the cached matrix value
+# setinv --> set the inverse matrix value
+# getinv --> get the cached inverse matrix
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix())
+{
+  invMat <- NULL
+  set <- function(y)
+  {
+    x <<- y
+    invMat <<- NULL
+  }
+  get <- function() x
+  setinv <- function(inv)
+  {
+    invMat <<- inv
+  }
+  getinv <- function() invMat
+  list(set = set, get = get, setinv = setinv, getinv = getinv)
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+# cacheSolve: This function computes the inverse of the special "matrix" returned by makeCacheMatrix above.
+# If the inverse has already been calculated (and the matrix has not changed), then the cacheSolve 
+# retrieves the inverse from the cache.
+# Otherwise it computes the inverse and caches it.
+cacheSolve <- function(x, ...)
+{
+  inv <- x$getinv()
+  if (!is.null(inv))
+  {
+    message("gettting cached value")
+    return(inv)
+  }
+  data <- x$get()
+  inv <- solve(data, ...)
+  x$setinv(inv)
+  inv
+  
 }
+
+# Test functions
+mat1 <- makeCacheMatrix() 
+mat2 <- mat1$get()
+mat2
+mat1$set(matrix(c(1,2,3,4,5,6,7,7,9), nrow=3, byrow=TRUE))
+mat2 <- mat1$get()
+mat2
+invmat2 <- cacheSolve(mat1)
+invmat2
+invmat2 <- cacheSolve(mat1)
+invmat2
+
+mat1$set(matrix(c(1,2,2,4,5,6,7,8,8), nrow=3, byrow=TRUE))
+mat2 <- mat1$get()
+mat2
+invmat2 <- cacheSolve(mat1)
+invmat2
+invmat2 <- cacheSolve(mat1)
+invmat2
